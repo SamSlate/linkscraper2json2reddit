10c245ea76167c38795912a7baf6c21d795ae681
samslate <samslate@mailinator.com>
2017-02-19 20:29:24 -0600

    refactoring

--- index.js
+++ index.js
@@ -16,110 +16,112 @@ var urls = JSON.parse(fs.readFileSync('config/urls.json', 'utf8'));
 //clock
 var runtime = new Date().getTime();
 
+//
+var redditPostArray = [];
+var count = Object.keys(urls).length;
+
 //list urls
 for(var n in urls){
+	// if(n == "CBP") getJson(n, urls[n]);	
 	getJson(n, urls[n]);	
 }
 
 function getJson(name, url){
 	console.log(name, "getJson()", "runtime: ", (new Date().getTime() - runtime)/1000);
 
-	// console.log(process._getActiveHandles());
-	// console.log(process._getActiveRequests());
-
-	// fs.readFile('json/'+name+'.json', 'utf8', function readFileCallback(err, data){
-	// try {
-	// 	var data = fs.readFileSync('json/'+name+'.json', 'utf8');
-	// 	compareAndAdd(linkArray, JSON.parse(data).linkArray, name);
-	// } catch (e) {
-	// 	console.log(name+'.json', " is NEW .json", e);
-	// 	compareAndAdd(linkArray, {linkArray: []}.linkArray, name);
-	// }
-	// return;
-
 	var readSpeed = new Date().getTime();
 	//too async for this world
 	fs.readFile('json/'+name+'.json', 'utf8', function(err, data){
-		if(err && err.errno != -4058) throw err;
+		if(err && err.errno != -4058){
+			throw err;
+		}
 		if(err && err.errno == -4058){
 			//file DNE
 			console.log(name+'.json', "is NEW .json");
-			compareAndAdd(url, {linkArray: []}.linkArray, name);
+			getPage(url, {linkArray: []}.linkArray, name);
 		}
 		else{
 			//file EXISTS
 			console.log(name+'.json', "EXISTS");
-			compareAndAdd(url, JSON.parse(data).linkArray, name);
+			getPage(url, JSON.parse(data).linkArray, name);
 		}
 		console.log("  readSpeed: ", (new Date().getTime() - readSpeed)/1000);
 	});
 	return;
 }
 
-function compareAndAdd(url, arrOld, name){	
+function compare(name, arrNew, arrOld){
+	
+	if(!arrOld) console.log(name, "no old array!");
+	if(!arrOld) var arrOld = [];
+	if(!arrNew) var arrNew = [];
+		
+	console.log(name, "compare()", arrOld.length);
 
-	getPage(name, url, function(arrNew){
-        console.log(name, "compareAndAdd()");
-        // console.log("compareAndAdd()", name, arrNew, arrOld);
-        if(!arrOld) var arrOld = [];
-        if(!arrNew) var arrNew = [];
-        
-        //compare and remove dups
-        var kill = 2;
-        function hasDup(value) {
-            for (var i = 0; i < arrOld.length; i++)		
-                if(value.href == arrOld[i].href)
-                    if(value.text.length <= arrOld[i].text.length) return false;
-            if(--kill > 0){
-                arrOld.push(value);
-                return true;
-            }
-            else return false;
-        }
-        // console.log(name, "arrOld.length: ", arrOld.length);
-        var rArr = arrNew.filter(hasDup);
-        // console.log(name, "arrOld.length: ", arrOld.length, rArr.length);
+	//compare and remove dups
+	var kill = 9;
+	function hasDup(value) {
+		for (var i = 0; i < arrOld.length; i++)		
+			if(value.href == arrOld[i].href)
+				if(value.text.length <= arrOld[i].text.length) return false;
+		if(--kill > 0){
+			arrOld.push(value);
+			redditPostArray.push({
+				name: name,
+				value: value
+			});
+			return true;
+		}
+		else return false;
+	}
+	// console.log(name, "arrOld.length: ", arrOld.length);
+	var rArr = arrNew.filter(hasDup);
+	// console.log(name, "arrOld.length: ", arrOld.length, rArr.length);
 
-        console.log("  "+name, rArr.length, "new!");
-        
-        //update existing array;
-        if(rArr.length > 0){
-            // postNewJobs(rArr);
-	        var readSpeed = new Date().getTime();   
-            fs.writeFileSync('json/'+name+'.json', JSON.stringify({linkArray: arrOld}));
-            // function (err) {
-                console.log("  writeSpeed: ", (new Date().getTime() - readSpeed)/1000);
-                //if (err) return console.log(err);
-                console.log("  "+name+'.json UPDATED');
-                console.log("  runtime: ", (new Date().getTime() - runtime)/1000);
-            // });
+	console.log("  "+name, rArr.length, "new! (", arrOld.length, "total)");
+	
+	//update existing array;
+	if(rArr.length > 0){
+		// postNewJobs(rArr);
+		var readSpeed = new Date().getTime();   
+		fs.writeFileSync('json/'+name+'.json', JSON.stringify({linkArray: arrOld}));
+		// function (err) {
+			console.log("  writeSpeed: ", (new Date().getTime() - readSpeed)/1000);
+			//if (err) return console.log(err);
+			console.log("  "+name+'.json UPDATED');
+			console.log("  runtime: ", (new Date().getTime() - runtime)/1000);
+		// });
 
-            //write sync
-            // console.log(fs.writeFileSync('json/'+name+'.json', JSON.stringify({linkArray: arrOld})));
-        }
-        else{
-            console.log("  "+name, "no changes, nothing to update");
-            console.log("  runtime: ", (new Date().getTime() - runtime)/1000);
-        } 
-        return;
-    });
-    return;
+		//write sync
+		// console.log(fs.writeFileSync('json/'+name+'.json', JSON.stringify({linkArray: arrOld})));
+	}
+	else{
+		console.log("  "+name, "no changes, nothing to update");
+		console.log("  runtime: ", (new Date().getTime() - runtime)/1000);
+	}
+	console.log(name, count, redditPostArray.length);
+
+	count--;
+	if(count==0){ //conclude program
+		//post array to reddit
+		// console.log(redditPostArray);
+		postNewJobs(redditPostArray);
+	}
+	return;
 }
-
-//get page
-function getPage(name, url, cb){
-    
+//get page	
+function getPage(url, arrOld, name){
     var readSpeed = new Date().getTime();
 	request(url, function (error, response, body) {
 		if (!error && response.statusCode == 200) {
 			console.log(name, response.statusCode);
             console.log("  requestSpeed: ", (new Date().getTime() - readSpeed)/1000);
-			//console.log(body) // Show the HTML for the Google homepage. 
-			cb(getAnchorEl(body));
+			//console.log(body) // Show the HTML for the Google homepage.
+			compare(name, getAnchorEl(body), arrOld);
 		}
 		else console.log(name, error, response); //POST THIS TO PAGE
 	});
-	return;
+    return;
 }
 
 //get links
@@ -137,7 +139,7 @@ function getAnchorEl(body){
 			linkArray.push({
 				href: el.href,
 				class: el.className,
-				text: el.textContent,
+				text: el.textContent.replace(/[^a-zA-Z0-9 -]/g," ").replace(/\s\s+/g, ' '),
 				target: el.target
 			});
 		}, this);
@@ -149,17 +151,21 @@ function getAnchorEl(body){
 function postNewJobs(arr){
 	if(!arr) return;   
     var r = new snoowrap(snooConfig);
-
 	for (var i = 0; i < arr.length; i++){
-		if(arr[i].text == undefined || arr[i].href[0] != 'h'){
-            console.log("post rejected: ", arr[i].text, arr[i].href);
-            break;
-        } 
-        console.log("letspost: ", arr[i].text, arr[i].href);
-        r.getSubreddit('LawJobsSydney').submitLink({
-            title: arr[i].text,
-            url: arr[i].href
-        });
+		var name = arr[i].name;
+		var title = "["+name+"] "+arr[i].value.text;
+		var url = arr[i].value.href;
+
+		if(title == undefined || (title.length-name.length) < 5 || url[0] != 'h'){
+            console.log("post rejected: ", title, url);
+        }
+		else{
+			console.log("letspost: ", title, url);
+			// r.getSubreddit('LawJobsSydney').submitLink({
+			//     title: title,
+			//     url: url
+			// });
+		}
     }
 	return;
 }
